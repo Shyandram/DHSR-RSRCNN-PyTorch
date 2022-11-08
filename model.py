7dad28583a0aa3d76c7bdf7d72b170006bcc5b94
shyandram <shyangen104@gmail.com>
2022-11-09 00:11:06 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,74 @@
+import math
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class AODnet(nn.Module):
+    def __init__(self):
+        super(AODnet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=1, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=6, out_channels=3, kernel_size=5, stride=1, padding=2)
+        self.conv4 = nn.Conv2d(in_channels=6, out_channels=3, kernel_size=7, stride=1, padding=3)
+        self.conv5 = nn.Conv2d(in_channels=12, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.b = 1
+
+    def forward(self, x):
+        x1 = F.relu(self.conv1(x))
+        x2 = F.relu(self.conv2(x1))
+        cat1 = torch.cat((x1, x2), 1)
+        x3 = F.relu(self.conv3(cat1))
+        cat2 = torch.cat((x2, x3), 1)
+        x4 = F.relu(self.conv4(cat2))
+        cat3 = torch.cat((x1, x2, x3, x4), 1)
+        k = F.relu(self.conv5(cat3))
+
+        if k.size() != x.size():
+            raise Exception("k, haze image are different size!")
+
+        output = k * x - k + self.b
+        return F.relu(output)
+
+class SRCNN(nn.Module):
+    def __init__(self) -> None:
+        super(SRCNN, self).__init__()
+        # Feature extraction layer.
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, (9, 9), (1, 1), (4, 4)),
+            nn.ReLU(True)
+        )
+
+        # Non-linear mapping layer.
+        self.map = nn.Sequential(
+            nn.Conv2d(64, 32, (5, 5), (1, 1), (2, 2)),
+            nn.ReLU(True)
+        )
+
+        # Rebuild the layer.
+        self.reconstruction = nn.Conv2d(32, 1, (5, 5), (1, 1), (2, 2))
+
+        # Initialize model weights.
+        self._initialize_weights()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._forward_impl(x)
+
+    # Support torch.script function.
+    def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.features(x)
+        out = self.map(out)
+        out = self.reconstruction(out)
+
+        return out
+
+    # The filter weight of each layer is a Gaussian distribution with zero mean and
+    # standard deviation initialized by random extraction 0.001 (deviation is 0)
+    def _initialize_weights(self) -> None:
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.normal_(module.weight.data, 0.0, math.sqrt(2 / (module.out_channels * module.weight.data[0][0].numel())))
+                nn.init.zeros_(module.bias.data)
+
+        nn.init.normal_(self.reconstruction.weight.data, 0.0, 0.001)
+        nn.init.zeros_(self.reconstruction.bias.data)
